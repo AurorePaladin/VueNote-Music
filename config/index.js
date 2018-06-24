4587433d09baf7bf8e29621f3099a5d06e0ab1f5
AurorePaladin <aurorepaladin@gmail.com>
2018-06-25 00:09:47 +0800

    github pages 打包上线

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     port: 9000,
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../docs/index.html'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
     productionSourceMap: true,
